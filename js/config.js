0cca08bcaac362b1110622fa063d09e6aa7f32d4
RDannn <rafiu.dania@yahoo.com>
2024-12-08 10:06:58 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_luT2dc7q2', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3k9u39pnbf90e0flo4esfgkuie', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_0KICX49Jq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ktl3jcp7cfeh8qi0g8mbm9f8c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
